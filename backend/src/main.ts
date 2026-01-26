aa5de3feef8d1bd452eb8545e181022fbe523f58
oohba <oohba@student.42tokyo.jp>
2026-01-26 18:32:40 +0900

    Corrected email regex pattern

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+  const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+  await app.listen(port);
 }
 bootstrap();
